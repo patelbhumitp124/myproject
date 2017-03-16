8f984a005d040933f5fbe3cdff45ddd2c6211b9a
patelbhumitp124 <patelbhumitp124@gmail.com>
2017-03-16 14:50:49 -0400

    addded before validation

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -5,12 +5,13 @@
 
         var vm = this;
         console.log("Controller: register");
-        
+        vm.user = {};
 
         registerSvc.getCountriesList()
             .then(function (response) {
                 console.log(response);
                 vm.countriesList = response.data.countriesList;
+                vm.selectedCountry = vm.countriesList[15];
             })
             .catch(function (response) {
                 console.log(response);
